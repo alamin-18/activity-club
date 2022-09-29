ce8c876fcbbd788fefd7774f9af153d4dfef6d46
Md Al Amin <fpalamin6@gmail.com>
2022-09-29 17:04:50 +0600

    data add

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Card.css'
 
 const Card = ({handleButton,activity}) => {
-    const {img,name,time,age} = activity
+    const {img,name,time,age,about} = activity
     return (
         <div className='card-container'>
             <div className='card'>
